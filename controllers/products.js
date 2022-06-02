695569d1b45728002789859478cc3403d8d92ead
Youssef04544 <youssefbenyedder04@gmail.com>
2022-06-02 16:41:19 +0100

    Added fields in the query & pagination using limit and skip

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,16 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("-name price");
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10)
+    .skip(5);
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -20,14 +24,27 @@ const getAllProducts = async (req, res) => {
   }
   // console.log(queryObject);
   let result = Product.find(queryObject);
+  //sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
+  //select fields to show
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
-  res.status(200).json({ products, nbHits: products.length });
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
